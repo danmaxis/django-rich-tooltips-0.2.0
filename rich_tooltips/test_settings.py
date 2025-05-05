a2d10990fdfcfb17401217bd1a6198acd28c0d31
Danilo Heroso de Deus Pereira <danmaxis@gmail.com>
2025-05-05 17:28:23 -0300

    Create test_settings.py
    Dummy settings for unit tests

---
+++ rich_tooltips/test_settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rich_tooltips',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',  # Use in-memory database for tests
+    }
+}
+
+SECRET_KEY = 'test-key'
